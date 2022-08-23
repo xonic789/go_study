e255c6ef7b6e304ebda6e8951d8f329fd5c90cfe
xonic789 <xonic789@naver.com>
2022-08-23 18:55:35 +0900

    연산자 및 데이터 타입(크기)

---
+++ src/section5/number_operation1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n1 uint8 = 125
+	var n2 uint8 = 90
+
+	fmt.Println("ex 1 : ", n1+n2)
+	fmt.Println("ex 1 : ", n1-n2)
+	fmt.Println("ex 1 : ", n1*n2)
+	fmt.Println("ex 1 : ", n1/n2)
+	fmt.Println("ex 1 : ", n1%n2)
+	fmt.Println("ex 1 : ", n1<<n2)
+	fmt.Println("ex 1 : ", n1>>n2)
+	fmt.Println("ex 1 : ", ^n1)
+
+}
